8392619a97b4806bb241fa36a21c0bf540e278f6
xavibou <xavibouhae@gmail.com>
2024-04-15 04:49:04 +0000

    Add mask generation and image processing functionality

--- mask_generators.py
+++ mask_generators.py
@@ -1,5 +1,7 @@
 import numpy as np
+import skimage
 import os
+import cv2
 from pathlib import Path
 from sklearn.decomposition import PCA
 from torchvision.ops.boxes import batched_nms
@@ -77,7 +79,9 @@ class SAMPaper:
                     stability_score.cpu(),
                 )
             stability_scores.append(stability_score.reshape(-1))
-            masks = masks if self.logits else self.predictor.model.mask_threshold < masks
+            masks = (
+                masks if self.logits else self.predictor.model.mask_threshold < masks
+            )
             maskss.append(masks.reshape(-1, *masks.shape[2:]))
             iou_predss.append(iou_preds.reshape(-1))
             print(".")
@@ -91,7 +95,9 @@ class SAMPaper:
         masks, iou_preds = maskss, iou_predss
         torch.cuda.empty_cache()
 
-        boxes = batched_mask_to_box(self.predictor.model.mask_threshold < masks if self.logits else masks)
+        boxes = batched_mask_to_box(
+            self.predictor.model.mask_threshold < masks if self.logits else masks
+        )
         keep_by_nms = batched_nms(
             boxes.float(),
             iou_preds,
@@ -113,7 +119,7 @@ class SAMPaper:
             sorted_indices = scores.argsort(descending=True)
             masks = masks[sorted_indices]
             # keep only the top mask_proposals
-            masks = masks[:self.max_proposals]
+            masks = masks[: self.max_proposals]
 
         print("processed!")
         return [
@@ -121,40 +127,79 @@ class SAMPaper:
             for mask, box in zip(masks, boxes)
         ]
 
+
+def rgb_to_int(image):
+    # Assumes image is numpy array of shape (H, W, 3) with dtype uint8
+    int_image = (
+        image[:, :, 0].astype(np.int32) * 256 * 256
+        + image[:, :, 1].astype(np.int32) * 256
+        + image[:, :, 2].astype(np.int32)
+    )
+    return int_image
+
+
 class MSSam(SAMPaper):
-    def __init__(self, *args, pca_dim=256, pamss_path="pamms/build/pamss", **kwargs):
-        assert kwargs['logits'] and kwargs['box_nms_thresh'] > 0.9, "MSSam requires logits and box_nms_thresh > 0.9"
+    def __init__(self, *args, pca_dim=256, pamss_path="pamss/build/pamss", **kwargs):
+        assert (
+            kwargs["logits"] and kwargs["box_nms_thresh"] > 0.9
+        ), "MSSam requires logits and box_nms_thresh > 0.9"
         super().__init__(*args, **kwargs)
         self.pca_dim = pca_dim
         self.pamss_path = pamss_path
 
-    def generate(self, img):
+    def generate(self, img, order=0):
         # get logits from the proposals
-        logits = torch.stack([m['segmentation'] for m in super().generate(img)], dim=0)
-        print('running pca...')
+        logits = torch.stack([m["segmentation"] for m in super().generate(img)], dim=0)
+        print("running pca...")
         C, H, W = logits.shape
-        logits = logits.reshape(C, H*W).T
+        logits = logits.reshape(C, H * W).T
         pca = PCA(n_components=self.pca_dim)
         pcafeats = pca.fit_transform(logits.cpu().numpy())
         pcafeats = pcafeats.reshape(H, W, self.pca_dim)
-        pcafeats = pcafeats.transpose(2, 0, 1).astype(np.float32)  # convert to format expected by MS
-        print('running Mumford-Shah...')
-        np.save('tmp.npy', pcafeats)  # temporary save, file-based communication
+        pcafeats = pcafeats.transpose(2, 0, 1).astype(
+            np.float32
+        )  # convert to format expected by MS
+        print("running Mumford-Shah...")
+        np.save("tmp.npy", pcafeats)  # temporary save, file-based communication
         # now we call pamss. Signature is ./pamsspath <input.npy> <output.png> <labels.png>, we call it for different levels, in a geometric way
-        n_masks = [self.max_proposals // (2**i) for i in range(1, int(np.floor(np.log2(self.max_proposals))))]
+        n_masks = [
+            self.max_proposals // (2**i)
+            for i in range(1, int(np.floor(np.log2(self.max_proposals))))
+        ]
         for n in n_masks:
-            call_command = f"./{self.pamss_path} -n {n} tmp.npy tmp.png labels_{n}.png"
+            call_command = (
+                f"./{self.pamss_path} -o {order} -n {n} tmp.npy tmp.png labels_{n}.png"
+            )
             print(f"running: `{call_command}`")
-            breakpoint()
             os.system(call_command)
-            print('done!')
+            print(f"done with `{call_command}!")
+        masks = []
+        for n in n_masks[::-1]:
+            rgb_labels = cv2.imread(f"labels_{n}.png")
+            labels = skimage.measure.label(rgb_to_int(rgb_labels))
+            assert (
+                labels.max() + 1 <= n
+            ), f"expected #labels<{n}, got {labels.max()+1} labels"
+            for i in range(n):
+                mask = labels == i
+                masks.append(mask)
+        masks = torch.from_numpy(np.array(masks))
+        boxes = batched_mask_to_box(masks)
+        return [
+            {"segmentation": mask, "bbox": box_xyxy_to_xywh(box).tolist()}
+            for mask, box in zip(masks, boxes)
+        ]
 
-        
 
-def tryit(imgpath, scales=[0, 1, 2], points_per_batch=256, use_cpu=False, box_nms_threshold=0.9, logits=False):
+def tryit(
+    imgpath,
+    scales=[0, 1, 2],
+    points_per_batch=256,
+    use_cpu=False,
+    box_nms_threshold=0.9,
+    logits=False,
+):
     # test the generator on cat.png
-    import cv2
-
     imgpath = Path(imgpath)
     # img = cv2.imread('cat.png')
     img = cv2.imread(str(imgpath))
@@ -169,12 +214,14 @@ def tryit(imgpath, scales=[0, 1, 2], points_per_batch=256, use_cpu=False, box_nm
     #     sam, points_per_batch=points_per_batch, scales=scales, box_nms_thresh=nms, use_cpu=use_cpu, logits=logits
     # )
     mask_generator = MSSam(
-        sam, points_per_batch=points_per_batch, scales=scales, box_nms_thresh=box_nms_threshold, use_cpu=use_cpu, logits=logits
+        sam,
+        points_per_batch=points_per_batch,
+        scales=scales,
+        box_nms_thresh=box_nms_threshold,
+        use_cpu=use_cpu,
+        logits=logits,
     )
     masks = mask_generator.generate(img)
-    # H, W = masks[0]["segmentation"].shape
-    # C = len(masks)
-    # print("masks shape: ", C, H, W)
 
 
 def run_pca(imgpath, masks, shape, scales, n_components):
